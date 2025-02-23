af99b80b728d2396be41456f1763f4e01f4ce56a
faifan143 <faisalfansa.bussiness@gmail.com>
2025-02-23 17:11:38 +0300

    access token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,20 +1,18 @@
 // src/auth/auth.controller.ts
 import {
-  Controller,
-  Post,
-  Body,
-  Res,
-  Get,
-  UseGuards,
   BadRequestException,
+  Body,
   ConflictException,
+  Controller,
+  Get,
   InternalServerErrorException,
+  Post,
+  UseGuards,
 } from '@nestjs/common';
-import { Response } from 'express';
 import { AuthService } from './auth.service';
-import { SignUpDto, SignInDto } from './dtos/auth.dto';
-import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { CurrentUser } from './decorators/current-user.decorator';
+import { SignInDto, SignUpDto } from './dtos/auth.dto';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { CurrentUserType } from './types/current-user.type';
 
 @Controller('auth')
@@ -22,20 +20,9 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  async signUp(
-    @Body() dto: SignUpDto,
-    @Res({ passthrough: true }) res: Response,
-  ) {
+  async signUp(@Body() dto: SignUpDto) {
     try {
       const user = await this.authService.signUp(dto);
-      const token = await this.authService.generateToken(user.id, user.phone);
-
-      res.cookie('token', token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: 'lax',
-        expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-      });
 
       return { message: 'User created successfully', user };
     } catch (error) {
@@ -49,29 +36,16 @@ export class AuthController {
     }
   }
   @Post('signin')
-  async signIn(
-    @Body() dto: SignInDto,
-    @Res({ passthrough: true }) res: Response,
-  ) {
-    const { token, user } = await this.authService.signIn(dto);
-    res.cookie('token', token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'lax',
-      expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-    });
-    return { message: 'Signed in successfully', data: { ...user } };
+  async signIn(@Body() dto: SignInDto) {
+    const { user } = await this.authService.signIn(dto);
+    const token = await this.authService.generateToken(user.id, user.phone);
+
+    return { message: 'Signed in successfully', data: { ...user, token } };
   }
 
   @Post('signout')
   @UseGuards(JwtAuthGuard)
-  async signOut(@Res({ passthrough: true }) res: Response) {
-    res.clearCookie('token', {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'lax',
-      path: '/',
-    });
+  async signOut() {
     return { message: 'Signed out successfully' };
   }
 
